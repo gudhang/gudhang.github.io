ff451d71fdaab25d1f1ba7fe967832f0b3e35775
candrahesen <candrahesen@gmail.com>
2018-08-11 00:21:08 +0700

    add validator for form submission

--- js/form-submission-handler-pemateri.js
+++ js/form-submission-handler-pemateri.js
@@ -1,21 +1,81 @@
 
-  var $form = $('form#pemateri-form'),
-  url = 'https://script.google.com/macros/s/AKfycbwrPpoteFJUiNugy-CqPv-plU_EjIm309zfZ703JiktuxRxDPU/exec'
+var $form = $('form#pemateri-form'),
+url = 'https://script.google.com/macros/s/AKfycbwrPpoteFJUiNugy-CqPv-plU_EjIm309zfZ703JiktuxRxDPU/exec'
 
 $(document).ready(function() {
-$('#submit-pemateri-form').on('click', function(e) {
-    e.preventDefault();
-    $('#registrasi-pemateri-form').modal('hide');
-    var jqxhr = $.ajax({
-      url: url,
-      method: "POST",
-      dataType: "json",
-      data: $form.serialize(),
-      success: function (response) {
-        $('#thankyou-message1').modal('show');
-      },
-    }).fail(function(response) {
-          alert("Failed");
-      });
-});
+
+  $('form#pemateri-form').bootstrapValidator({       
+    fields: {
+        pemateri_fullname: {
+         message: 'Nama Lengkap tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Nama Lengkap tidak boleh kosong'
+                },
+            }
+        },
+        pemateri_phone: {
+            message: 'Nomor Telepon Whatsapp tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Nomor Telepon Whatsapp tidak boleh kosong'
+                }
+            }
+        },
+        pemateri_email: {
+            validators: {
+                notEmpty: {
+                    message: 'Masukkan alamat email Anda'
+                },
+                emailAddress: {
+                    message: 'Alamat email tidak valid'
+                }
+            }
+        },
+        pemateri_schools: {
+            message: 'Afiliasi tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Afiliasi tidak boleh kosong'
+                }
+            }
+        }, 
+        pemateri_address: {
+            message: 'Alamat tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Alamat tidak boleh kosong'
+                }
+            }
+        },
+        pemateri_reason: {
+            message: 'Alasan bergabung tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Silahkan isi alasan Anda bergabung dengan kami'
+                }
+            }
+        },
+
+    }
+  })
+  .on('success.form.bv', function(e) {
+      e.preventDefault();
+
+      var bv = $form.data('bootstrapValidator');
+
+      $('#registrasi-pemateri-form').modal('hide');
+      var jqxhr = $.ajax({
+        url: url,
+        method: "POST",
+        dataType: "json",
+        data: $form.serialize(),
+        success: function (response) {
+          $('#thankyou-message1').modal('show');
+        },
+      }).fail(function(response) {
+            alert("Failed");
+        });
+
+  });
 });
--- js/form-submission-handler-siswa.js
+++ js/form-submission-handler-siswa.js
@@ -1,21 +1,81 @@
 
-  var $formSiswa = $('form#siswa-form'),
-  urlSiswa = 'https://script.google.com/macros/s/AKfycbzmfxmVog56ucw8evHP_Cr7pyPXGdQnYUb5qgRwRGPGaK97_aU/exec'
+var $formSiswa = $('form#siswa-form'),
+urlSiswa = 'https://script.google.com/macros/s/AKfycbzmfxmVog56ucw8evHP_Cr7pyPXGdQnYUb5qgRwRGPGaK97_aU/exec'
 
 $(document).ready(function() {
-$('#submit-siswa-form').on('click', function(e) {
-    e.preventDefault();
-    $('#registrasi-siswa-form').modal('hide');
-    var jqxhr = $.ajax({
-      url: urlSiswa,
-      method: "POST",
-      dataType: "json",
-      data: $formSiswa.serialize(),
-      success: function (response) {
-        $('#thankyou-message1').modal('show');
-      },
-    }).fail(function(response) {
-          alert("Failed");
-      });
-});
+
+  $('form#siswa-form').bootstrapValidator({       
+    fields: {
+        siswa_fullname: {
+         message: 'Nama Lengkap tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Nama Lengkap tidak boleh kosong'
+                },
+            }
+        },
+        siswa_phone: {
+            message: 'Nomor Telepon Whatsapp tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Nomor Telepon Whatsapp tidak boleh kosong'
+                }
+            }
+        },
+        siswa_email: {
+            validators: {
+                notEmpty: {
+                    message: 'Masukkan alamat email Anda'
+                },
+                emailAddress: {
+                    message: 'Alamat email tidak valid'
+                }
+            }
+        },
+        siswa_schools: {
+            message: 'Afiliasi tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Afiliasi tidak boleh kosong'
+                }
+            }
+        }, 
+        siswa_address: {
+            message: 'Alamat tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Alamat tidak boleh kosong'
+                }
+            }
+        },
+        siswa_reason: {
+            message: 'Alasan bergabung tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Silahkan isi alasan Anda bergabung dengan kami'
+                }
+            }
+        },
+
+    }
+  })
+  .on('success.form.bv', function(e) {
+      e.preventDefault();
+
+      var bv = $formSiswa.data('bootstrapValidator');
+      
+      $('#registrasi-siswa-form').modal('hide');
+      var jqxhr = $.ajax({
+        url: urlSiswa,
+        method: "POST",
+        dataType: "json",
+        data: $formSiswa.serialize(),
+        success: function (response) {
+          $('#thankyou-message1').modal('show');
+        },
+      }).fail(function(response) {
+            alert("Failed");
+        });
+
+  });
 });
