6ae9ffa02cb1ea7f64cb9df6324a463aa21c723a
candrahesen <candrahesen@gmail.com>
2018-08-11 00:39:31 +0700

    add validator for form submission

--- js/form-submission-handler-donasi-buku.js
+++ js/form-submission-handler-donasi-buku.js
@@ -1,21 +1,71 @@
 
-  var $formDonasiBuku = $('form#donasi-buku-form'),
-  urlDonasiBuku = 'https://script.google.com/macros/s/AKfycbwQ5911AZy-9fHBfZQnhGKVjKW-Q6Rgo4MxEg_eJnz-8XPvxOQ/exec'
+var $formDonasiBuku = $('form#donasi-buku-form'),
+urlDonasiBuku = 'https://script.google.com/macros/s/AKfycbwQ5911AZy-9fHBfZQnhGKVjKW-Q6Rgo4MxEg_eJnz-8XPvxOQ/exec'
 
 $(document).ready(function() {
-$('#submit-donasi-buku-form').on('click', function(e) {
-    e.preventDefault();
-    $('#donasi-buku-form').modal('hide');
-    var jqxhr = $.ajax({
-      url: urlDonasiBuku,
-      method: "POST",
-      dataType: "json",
-      data: $formDonasiBuku.serialize(),
-      success: function (response) {
-        $('#thankyou-message-donasi').modal('show');
-      },
-    }).fail(function(response) {
-          alert("Failed");
-      });
-});
+
+  $('form#donasi-buku-form').bootstrapValidator({       
+    fields: {
+        donatur_fullname: {
+         message: 'Nama Lengkap tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Nama Lengkap tidak boleh kosong'
+                },
+            }
+        },
+        donatur_phone: {
+            message: 'Nomor Telepon Whatsapp tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Nomor Telepon Whatsapp tidak boleh kosong'
+                }
+            }
+        },
+        donatur_address: {
+            message: 'Alamat tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Alamat tidak boleh kosong'
+                }
+            }
+        },
+        daftar_sumbangan_buku: {
+            message: 'Buku yang didonasikan tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Silahkan isi buku yang ingin Anda donasikan'
+                }
+            }
+        },
+        donatur_reason: {
+            message: 'Alasan mendonasikan buku tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Silahkan isi alasan Anda mendonasikan buku'
+                }
+            }
+        },
+
+    }
+  })
+  .on('success.form.bv', function(e) {
+      e.preventDefault();
+
+      var bv = $formDonasiBuku.data('bootstrapValidator');
+
+      $('#donasi-buku-form').modal('hide');
+      var jqxhr = $.ajax({
+        url: urlDonasiBuku,
+        method: "POST",
+        dataType: "json",
+        data: $formDonasiBuku.serialize(),
+        success: function (response) {
+          $('#thankyou-message-donasi').modal('show');
+        },
+      }).fail(function(response) {
+            alert("Failed");
+        });
+
+  });
 });
--- js/form-submission-handler-peminjaman-buku.js
+++ js/form-submission-handler-peminjaman-buku.js
@@ -3,19 +3,86 @@ var $formBorrowedBook = $('form#peminjam-form'),
 urlBorrowedBook = 'https://script.google.com/macros/s/AKfycbzdaD-6vEOYQKADxCHFv_EKykPBjbUjiCYk5nMLHARyoi-GIz-G/exec'
 
 $(document).ready(function() {
-$('#submit-peminjaman-form').on('click', function(e) {
-    e.preventDefault();
-    $('#meminjam-buku-form').modal('hide');
-    var jqxhr = $.ajax({
-      url: urlBorrowedBook,
-      method: "POST",
-      dataType: "json",
-      data: $formBorrowedBook.serialize(),
-      success: function (response) {
-        $('#thankyou-message-meminjam').modal('show');
-      },
-    }).fail(function(response) {
-          alert("Failed");
-      });
-});
+
+  $('form#peminjam-form').bootstrapValidator({       
+    fields: {
+        peminjam_fullname: {
+         message: 'Nama Lengkap tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Nama Lengkap tidak boleh kosong'
+                },
+            }
+        },
+        peminjam_phone: {
+            message: 'Nomor Telepon Whatsapp tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Nomor Telepon Whatsapp tidak boleh kosong'
+                }
+            }
+        },
+        peminjam_address: {
+            message: 'Alamat Rumah tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Alamat Rumah tidak boleh kosong'
+                }
+            }
+        },
+        peminjam_id: {
+            message: 'Nomor Keanggotaan tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Nomor Keanggotaan tidak boleh kosong'
+                }
+            }
+        },
+        peminjam_book_id: {
+            message: 'Kode Buku tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Kode Buku tidak boleh kosong'
+                }
+            }
+        },
+        lama_peminjaman: {
+            message: 'Lama Peminjaman tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Lama Peminjaman tidak boleh kosong'
+                }
+            }
+        },
+        peminjam_reason: {
+            message: 'Alasan meminjam buku tidak valid',
+            validators: {
+                notEmpty: {
+                    message: 'Silahkan isi alasan Anda meminjam buku'
+                }
+            }
+        },
+
+    }
+  })
+  .on('success.form.bv', function(e) {
+      e.preventDefault();
+
+      var bv = $formBorrowedBook.data('bootstrapValidator');
+
+      $('#meminjam-buku-form').modal('hide');
+      var jqxhr = $.ajax({
+        url: urlBorrowedBook,
+        method: "POST",
+        dataType: "json",
+        data: $formBorrowedBook.serialize(),
+        success: function (response) {
+          $('#thankyou-message-meminjam').modal('show');
+        },
+      }).fail(function(response) {
+            alert("Failed");
+        });
+
+  });
+
 });
